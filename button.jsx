68796a25670d7b87230eb6bae92e492e59e4b6bd
Saraswatipatil20 <Saraswatipatil2004@gmail.com>
2026-02-26 13:02:11 +0530

    Add Button component with customizable props

---
+++ button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ children, className, ...props }) => {
+    return (
+      <button
+        className={`px-4 py-2 rounded-md font-semibold focus:outline-none ${className}`}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  };
+  
+  export { Button };
+  
